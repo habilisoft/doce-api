f55e86b8a24a5047567a159b5eafd0afa0be5e75
Armando Perez <armando@yoursuppliernetwork.com>
2022-12-19 07:53:24 -0400

    Create users

--- src/main/java/com/habilisoft/doce/api/auth/dto/CreateUserRequest.java
+++ src/main/java/com/habilisoft/doce/api/auth/dto/CreateUserRequest.java
@@ -1,7 +1,9 @@
 package com.habilisoft.doce.api.auth.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.Map;
 
@@ -10,6 +12,8 @@ import java.util.Map;
  */
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class CreateUserRequest {
     private String email;
     private String password;
